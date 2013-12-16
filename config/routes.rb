4697627dd620286449b426dd2c06059a077724ca
Andy Wang <wangyaodi@gmail.com>
2013-12-16 14:38:40 +0800

    add default root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 WeixinDemo::Application.routes.draw do
+  
+  root 'people#index'
+
   resources :people
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
